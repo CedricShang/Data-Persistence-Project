1e3ef25d9cc25757ab226817c8387130342dfa2c
CedricShang <48117002+CedricShang@users.noreply.github.com>
2021-09-09 16:49:00 +0800

    feat: finished start menu, add a data management and etc

---
+++ Assets/Scripts/Data/SaveData.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SaveData
+{
+    [System.Serializable]
+    public struct PlayRecord
+    {
+        public string m_Name;
+        public int m_Score;
+    }
+
+    
+    
+    public string ToJson()
+    {
+        return JsonUtility.ToJson(this);
+    }
+
+    public void LoadFromJson(string a_Json)
+    {
+        JsonUtility.FromJsonOverwrite(a_Json, this);
+    }
+}
+
+public interface ISaveable
+{
+    void PopulateSaveData(SaveData a_SaveData);
+    void LoadFromSaveData(SaveData a_SaveData);
+}
--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,76 +1,69 @@
+using System;
+using System.ComponentModel;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
-
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 public class MainManager : MonoBehaviour
 {
-    public Brick BrickPrefab;
-    public int LineCount = 6;
-    public Rigidbody Ball;
-
-    public Text ScoreText;
-    public GameObject GameOverText;
-    
-    private bool m_Started = false;
-    private int m_Points;
-    
-    private bool m_GameOver = false;
-
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        const float step = 0.6f;
-        int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
-        for (int i = 0; i < LineCount; ++i)
-        {
-            for (int x = 0; x < perLine; ++x)
-            {
-                Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
-                var brick = Instantiate(BrickPrefab, position, Quaternion.identity);
-                brick.PointValue = pointCountArray[i];
-                brick.onDestroyed.AddListener(AddPoint);
-            }
+    public TMP_InputField NameInputField= null;
+    public Animator NameEmptyWarning = null;
+    public string PlayerName{get;set;}
+    private int highScore;
+    public int HighScore{get=>highScore;set{
+        NewHighScore?.Invoke();
+    }}
+    public float duration = 1.5f;
+    /// <summary>
+    /// Singleton
+    /// </summary>
+    static public MainManager Instance = null;
+    private MainManager(){}
+    private void Awake() {
+        if (Instance != null){
+            Destroy(this.gameObject);
+            return;
         }
+        Instance = this;
+        DontDestroyOnLoad(Instance);     
     }
 
-    private void Update()
-    {
-        if (!m_Started)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                m_Started = true;
-                float randomDirection = Random.Range(-1.0f, 1.0f);
-                Vector3 forceDir = new Vector3(randomDirection, 1, 0);
-                forceDir.Normalize();
-
-                Ball.transform.SetParent(null);
-                Ball.AddForce(forceDir * 2.0f, ForceMode.VelocityChange);
-            }
-        }
-        else if (m_GameOver)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-            }
+    private void Start() {
+        //for name input field
+        if (NameInputField == null)
+            NameInputField = GameObject.Find("InputField").GetComponent<TMP_InputField>();
+        if (NameEmptyWarning == null)
+            NameEmptyWarning = GameObject.Find("NameEmptyWarning/Text").GetComponent<Animator>();
+    }
+    //these 2 for menu
+    public void StartGame(){
+        //get name
+        Debug.Log("MainManager StartGame Clicked");
+        var nameStr = NameInputField.text;
+        if (nameStr== ""|| nameStr == "Your name?"){
+            //warning
+            Debug.Log("MainManager Warning");
+            NameEmptyWarning.SetTrigger("on");
+        }else{
+            Instance.PlayerName=nameStr;
+            SceneManager.LoadScene("main",LoadSceneMode.Single);
         }
     }
-
-    void AddPoint(int point)
-    {
-        m_Points += point;
-        ScoreText.text = $"Score : {m_Points}";
+    
+    public void Quit(){
+        #if UNITY_EDITOR
+            EditorApplication.ExitPlaymode();
+        #else
+            Application.Quit();
+        #endif
     }
+    //this one for in game.
+    public void BackToMenu(){
+        SceneManager.LoadScene("Enter Menu",LoadSceneMode.Single);
 
-    public void GameOver()
-    {
-        m_GameOver = true;
-        GameOverText.SetActive(true);
     }
 }
